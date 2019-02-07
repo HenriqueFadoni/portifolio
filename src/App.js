e630e070b19eca87d783b71f142e665f34b4c14a
Henrique Fadoni <henriquefadonii@gmail.com>
2019-02-07 17:21:21 -0200

    Creating Text in the Welcome Section

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ class App extends Component {
     return (
       <>
         <section className="section-welcome">
-          <h1>Hello,</h1>
-          <h3>{`My name is Henrique Fadoni,\nI'm a Front-End Developer`}</h3>
+          <div className="welcome-text-container">
+            <div className="welcome-text-1">
+              <h1>Hello,</h1>
+            </div>
+            <div className="welcome-text-2">
+              <h3>My name is <span className="name-color">Henrique Fadoni</span>,</h3>
+              <h3>I'm a <span className="name-color">Front-End Developer</span></h3>
+            </div>
+          </div>
         </section>
         <section className="section-about"></section>
       </>
