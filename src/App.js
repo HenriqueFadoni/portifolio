2f30d533d88faad168f5a6cd20543957586f6cc3
Henrique Fadoni <henriquefadonii@gmail.com>
2019-02-07 12:37:22 -0200

    Adding a Google Font to the Project

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ class App extends Component {
     return (
       <>
         <section className="section-welcome">
+          <h1>Hello,</h1>
+          <h3>{`My name is Henrique Fadoni,\nI'm a Front-End Developer`}</h3>
         </section>
         <section className="section-about"></section>
       </>
