1288a71f2255fce148efc35f094e457c0ef5e3c6
Henrique Fadoni <henriquefadonii@gmail.com>
2019-02-10 15:45:01 -0200

    Creating NavigationBar

--- src/containers/Menu/Menu.js
+++ src/containers/Menu/Menu.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 class Menu extends Component {
     render() {
         return (
-            <div className="Menu">
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Portifolio</li>
-                    <li>Contact</li>
-                </ul>
+            <div className='navbar__container'>
+                <nav className='navbar__nav'>
+                    <ul>
+                        <li><a href='#section-01'>Home</a></li>
+                        <li><a href='#section-02'>About</a></li>
+                        <li><a href='/'>Portifolio</a></li>
+                        <li><a href='/'>Contact</a></li>
+                    </ul>
+                </nav>
             </div>
         );
     }
