da7d4b878efe627e1e70db7dfc1ad97ac47d7a14
Henrique Fadoni <henriquefadonii@gmail.com>
2019-02-13 19:52:14 -0200

    Creating Items inside Portifolio grid

--- src/components/PortifolioItem/PortifolioItem.js
+++ src/components/PortifolioItem/PortifolioItem.js
@@ -3,9 +3,10 @@ import React from 'react';
 const PortifolioItem = props => {
     return (
         <>
-            <div className={`grid-row-portifolio__1-of-4 portifolio__${props.classN}`}>
+            <div className={`grid-row-portifolio__1-of-4 item-${props.classN}`}>
                 <h2>{props.title}</h2>
                 <h3>{props.description}</h3>
+                <button className='btn-portifolio'>LEARN MORE</button>
             </div>
         </>
     );
